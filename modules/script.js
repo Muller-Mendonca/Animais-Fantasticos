e007eb626401994617799a4025dc858f0fe6245a
Muller-Mendonca <mullerm.oficial@gmail.com>
2020-09-20 16:31:46 -0300

    update all

---
+++ modules/script.js
@@ -0,0 +1,23 @@
+import initScrollSuave from './scroll-suave.js';
+import initAnimationScroll from './scroll-animacao.js';
+import initAccordion from './accordion.js';
+import initTabNav from './tab-nav.js';
+import initModal from './modal.js';
+import initTooltip from './tooltip.js';
+import initDropdownMenu from './dropdown-menu.js';
+import initMenuMobile from './menu-mobile.js';
+import initAnimaNumeros from './anima-numeros.js';
+import initFuncionamento from './funcionamento.js';
+
+
+
+initScrollSuave();
+initAnimationScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
