56912a4ae725b6f49b65d3a9a2fcb71b28862326
Muller-Mendonca <mullerm.oficial@gmail.com>
2020-10-04 20:40:30 -0300

    final update

--- modules/scroll-suave.js
+++ modules/scroll-suave.js
@@ -25,8 +25,10 @@ export default class ScrollSuave{
     }
      
     init(){
-        console.log('ok')
-        this.addLinkEvent();
+        if(this.linksInternos.length){
+            this.addLinkEvent();
+        }
+        return this;
     } 
 }
     
