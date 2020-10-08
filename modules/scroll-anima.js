d553bda6f60d141d80e04b12943d0c7d60513f9b
Muller-Mendonca <mullerm.oficial@gmail.com>
2020-10-08 16:13:19 -0300

    não concluido

--- modules/scroll-anima.js
+++ modules/scroll-anima.js
@@ -3,23 +3,44 @@ export default class ScrollAnima{
         this.sections = document.querySelectorAll(sections);
         this.windowMetade = window.innerHeight * 0.6;
 
-        this.animaScroll = this.animaScroll.bind(this);
+        this.checkDistance = this.checkDistance.bind(this);
+    }
+
+    getDistance(){
+        this.distance = [...this.sections].map((section) =>{
+            const offset = section.offsetTop;
+            return{
+                element: section,
+                offset,
+            };
+        });
+    }
+
+    //PAREI 15:30 AULA 10
+
+    checkDistance(){
+        this.distance.forEach((item) =>{
+            if(window.pageYOffset > item.offset){
+                item.element.classList.add('setativo');
+            }else if(item.element.classList.contains('setativo')){
+                item.element.classList.remove('setativo');
+            }
+        });
     }
 
      animaScroll(){
+         console.log('ativou')
          this.sections.forEach((section) => {
              const sectionTop = section.getBoundingClientRect().top;
              const isSectionVisible = (sectionTop - this.windowMetade) < 0
              if(isSectionVisible){
-                 section.classList.add('setativo');
-             }else if(section.classList.contains('setativo')){
-                section.classList.remove('setativo');
-             }
+                 
          });
      }
 
      init(){
-        window.addEventListener('scroll', this.animaScroll);
+         this.animaScroll();
+        window.addEventListener('scroll', this.checkDistance);
      }
 };
   
