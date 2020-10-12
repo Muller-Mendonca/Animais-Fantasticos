ca303d60a57e0e5b2ceddf7a9bc88be208c2c2d3
Muller-Mendonca <mullerm.oficial@gmail.com>
2020-10-12 16:19:15 -0300

    refatorando menuMobile

--- modules/menu-mobile.js
+++ modules/menu-mobile.js
@@ -1,11 +1,13 @@
 import outSideClick from './outsideclick.js';
 import outsideClick from './outsideclick.js';
 
-export default function initMenuMobile(){
-
-    const menuButton = document.querySelector('[data-menu="button"]');
-    const menuList = document.querySelector('[data-menu="list"]');
-    const eventos = ['click', 'touchstart'];
+export default class MenuMobile{
+    constructor(){
+        const menuButton = document.querySelector('[data-menu="button"]');
+        const menuList = document.querySelector('[data-menu="list"]');
+        const eventos = ['click', 'touchstart'];
+    }
+    
     
     if(menuButton){
     function openMenu(event){
