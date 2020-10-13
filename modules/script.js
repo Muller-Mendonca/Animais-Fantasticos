ce8f53ca8ad5e95a0f66fbe6dd7988820e09840b
Muller-Mendonca <mullerm.oficial@gmail.com>
2020-10-13 20:17:02 -0300

    refatorando funcionamento

--- modules/script.js
+++ modules/script.js
@@ -5,7 +5,7 @@ import Modal from './modal.js';
 import Tooltip from './tooltip.js';
 import DropdownMenu from './dropdown-menu.js';
 import MenuMobile from './menu-mobile.js';
-import initFuncionamento from './funcionamento.js';
+import Funcionamento from './funcionamento.js';
 import fetchAnimais from './fetch-animais.js';
 import fetchBitcoin from './fetch-bitcoin.js';
 import ScrollAnima from './scroll-anima.js';
@@ -34,7 +34,8 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]')
 menuMobile.init();
 
-initFuncionamento();
+const funcionamento = new Funcionamento('[data-semana]', 'aberto')
+funcionamento.init();
 
 fetchAnimais('../animaisapi.json', '.numeros-grid');
 
